bd4f5ebf1e864f2dccb7c00cb9f989724bfa8d6e
Арзамасов Е.В ТСА-141 <arzamasov13377@gmail.com>
2024-03-04 18:36:55 +0300

    Добавлено описание функции

--- Задание 1-1.cpp
+++ Задание 1-1.cpp
@@ -2,13 +2,18 @@
 using namespace std;
 
 void getResult(double x, double y, double z);
-
+/**
+* @breef - функция, производящая вычисление результата
+* param x - число х
+* param y - число y
+* param z - число z
+*/
 int main()
 {
     const double x = 1.825;
     const double y = 18.225;
     const double z = -3.298;
-    getResult(x, y,z);
+    getResult(x, y, z);
     return 1;
 }
 
@@ -16,5 +21,5 @@ void getResult(double x, double y,double z)
 {
     cout << "A=" << pow(x, (x / y)) - pow(y / x, 1 / 3) << endl;
     cout << "B=" << (y - x) + ((y - z) / (y - x)) / (3 + (pow(z, 2) / 5)) << endl;
-    exit(0);
+    abort();
 }
