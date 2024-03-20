92a7d3b8cc0ce3efe7627b262881a9118c7d861a
Арзамасов Егор Владимирович ТСА-141 <160852304+ArzamasovEV@users.noreply.github.com>
2024-03-20 18:50:28 +0300

    Add files via upload

--- Задание 3.cpp
+++ Задание 3.cpp
@@ -1,5 +1,4 @@
 ﻿#include <iostream>
-
 using namespace std;
 /**
 * @breef Считывает значение с клавиатуры с проверкой ввода
@@ -7,15 +6,15 @@ using namespace std;
 */
 double getValue();
 /**
- * @brief Проверяет условия прохождения кирпича через отверстие
- * @param x Длина кирпича
- * @param y Ширина кирпича
- * @param z Высота кирпича
- * @param r Длина отверстия
- * @param s Ширина отверстия
- * @return 0, если кирпич проходит, иначе 1
- */
-int checkConditions(double x, double y, double z, double r, double s);
+* @breef Принимает переменные и проверяет условие прохождения кирпича через отверстие.
+* @param x - длина кирпича.
+* @param y - ширина кирпича.
+* @param z - высота кирпича.
+* @param r - длина отверстия.
+* @param s - ширина отверстия.
+* @return Возвращает результат проверки условия.
+*/
+bool checkConditions(double x, double y, double z, double r, double s);
 /**
 * @breef - главная функция программы
 * @return - возвращает 0, если функция выполнена верно, иначе 1.
@@ -31,28 +30,34 @@ int main() {
     double r = getValue();
     cout << "Enter hole width = ";
     double s = getValue();
-    checkConditions(x, y, z, r, s);
 
-    return 0;
-}
-int checkConditions(double x, double y, double z, double r, double s) {
-    if ((x <= r && y <= s) || (x <= s && y <= r) ||
-        (x <= r && z <= s) || (x <= s && z <= r) ||
-        (y <= r && z <= s) || (y <= s && z <= r)) {
+    bool fit = checkConditions(x, y, z, r, s);
+
+    if (fit) {
         cout << "The brick goes through the hole." << endl;
-        return 0;
     }
     else {
         cout << "The brick does not fit through the hole." << endl;
-        return 1;
+    }
+
+    return 0;
+}
+
+bool checkConditions(double x, double y, double z, double r, double s) {
+    if ((x <= r && y <= s) || (x <= s && y <= r) ||
+        (x <= r && z <= s) || (x <= s && z <= r) ||
+        (y <= r && z <= s) || (y <= s && z <= r)) {
+        return true;
+    }
+    else {
+        return false;
     }
 }
-    double getValue()
-{
+
+double getValue() {
     double value;
     cin >> value;
-    if (cin.fail())
-    {
+    if (cin.fail() || value <= 0) {
         cout << "incorrect value" << endl;
         abort();
     }
